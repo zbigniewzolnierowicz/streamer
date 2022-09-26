3ddf8a3f90a180137441d689141c49685312369b
Zbigniew Żołnierowicz <zbigniew.zolnierowicz@gmail.com>
2022-09-26 20:26:33 +0200

    Move init to a function

--- apps/auth/index.ts
+++ apps/auth/index.ts
@@ -9,26 +9,29 @@ import { router as googleRoutes } from "./routes/google";
 dotenv.config();
 
 const port = process.env.PORT || 8080;
-DatabaseConnection.initialize();
 
-const app = express();
-app.use(cookieParser());
-app.use("/auth/google", googleRoutes);
+function bootstrap() {
+    DatabaseConnection.initialize();
 
-
-const server = app.listen(port, async () => {
-    console.log(`[server]: Server is running at https://localhost:${port}`);
-});
-
-process.on("SIGTERM", function () {
-    server.close(async function () {
-        await DatabaseConnection.destroy();
+    const app = express();
+    app.use(cookieParser());
+    app.use("/auth/google", googleRoutes);
+    const server = app.listen(port, async () => {
+        console.log(`[server]: Server is running at https://localhost:${port}`);
     });
 
-    setTimeout(function () {
-        console.error(
-            "Could not close connections in time, forcefully shutting down"
-        );
-        process.exit(1);
-    }, 30 * 1000);
-});
+    process.on("SIGTERM", function () {
+        server.close(async function () {
+            await DatabaseConnection.destroy();
+        });
+
+        setTimeout(function () {
+            console.error(
+                "Could not close connections in time, forcefully shutting down"
+            );
+            process.exit(1);
+        }, 30 * 1000);
+    });
+}
+
+bootstrap();
